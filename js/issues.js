267bff19daa981b07ecfb80834fcd460d0df0243
Skyler <skylertom@gmail.com>
2015-04-20 14:22:26 -0400

    Added selection of 1 judge

--- js/issues.js
+++ js/issues.js
@@ -1,6 +1,26 @@
 // Graph a justice's votes by issue
 
-var issueKeys = {1:"Criminal Procedure",
+$('.dropdown-toggle').dropdown();
+$('#dropdownList li').on('click', function() {
+    $('#dropdownMenu1').html($(this).html());
+    });
+
+var issueKeys = {
+"Criminal Procedure":1,
+"Civil Rights":2,
+"First Amendment":3,
+"Due Process":4,
+"Privacy":5,
+"Attorneys":6,
+"Unions":7,
+"Economic Activity":8,
+"Judicial Power":9,
+"Federalism":10,
+"Interstate Relations":11,
+"Federal Taxation":12,
+"Miscellaneous":13,
+"Private Action":14,
+1:"Criminal Procedure",
 2:"Civil Rights",
 3:"First Amendment",
 4:"Due Process",
@@ -20,12 +40,8 @@ var margin = {top: 20, right: 50, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-//var parseDate = d3.time.format("%d-%b-%y").parse,
 var parseDate = d3.time.format("%Y").parse,
     bisectDate = d3.bisector(function(d) { return d.date; }).left,
-    //CHANGE: this is what it will print out on hover for y axis
-    formatValue = d3.format(",.2f"),
-    formatCurrency = function(d) { return "$" + formatValue(d); },
     formatHoverText = function (d) {
       return "Voted in the Majority " + d.vote + " times out of " +
         d.totalvotes + " cases";
@@ -55,36 +71,35 @@ var svg = d3.select("body").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-//d3.csv("data/justice-centered/SCDB_2014_01_justiceCentered_Vote.csv", function(error, data) {
 d3.csv("data/justice-centered/SCDB_2014_01_justiceCentered_Vote.csv", function(error, csv_data) {
   csv_data.forEach(function(d) {
     d.term = parseDate(d.term);
     if (d.justiceName == "TMarshall"){ 
-    if (data.length == 0) {
-      var sccase = new Object();
-      sccase.date = d.term;
-      if (d.vote == 1 || d.vote == 3 || d.vote == 4) //only count consents
-        sccase.vote = 1;
-      else sccase.vote = 0;
-      sccase.totalvotes = 1;
-      data.push(sccase);
+      if (data.length == 0) {
+        var sccase = new Object();
+        sccase.date = d.term;
+        if (d.vote == 1 || d.vote == 3 || d.vote == 4) //only count consents
+          sccase.vote = 1;
+        else sccase.vote = 0;
+        sccase.totalvotes = 1;
+        data.push(sccase);
+      }
+      else if (data[data.length-1].date.getTime() != d.term.getTime()) {
+        var sccase = new Object();
+        sccase.date = d.term;
+        if (d.vote == 1 || d.vote == 3 || d.vote == 4) //only count consents
+          sccase.vote = 1;
+        else sccase.vote = 0;
+        sccase.totalvotes = 1;
+        data.push(sccase);
+      }
+      else {
+        d.vote = +d.vote;
+        if (d.vote == 1 || d.vote == 3 || d.vote == 4) //only count consents
+          data[data.length-1].vote += 1;
+        data[data.length-1].totalvotes += 1;
+      }
     }
-    else if (data[data.length-1].date.getTime() != d.term.getTime()) {
-      var sccase = new Object();
-      sccase.date = d.term;
-      if (d.vote == 1 || d.vote == 3 || d.vote == 4) //only count consents
-        sccase.vote = 1;
-      else sccase.vote = 0;
-      sccase.totalvotes = 1;
-      data.push(sccase);
-    }
-    else {
-      d.vote = +d.vote;
-      if (d.vote == 1 || d.vote == 3 || d.vote == 4) //only count consents
-        data[data.length-1].vote += 1;
-      data[data.length-1].totalvotes += 1;
-    }
-  }
   });
 
   data.sort(function(a, b) {
@@ -92,7 +107,6 @@ d3.csv("data/justice-centered/SCDB_2014_01_justiceCentered_Vote.csv", function(e
   });
 
   x.domain([data[0].date, data[data.length - 1].date]);
-  //y.domain(d3.extent(data, function(d) { return d.close; }));
   y.domain(d3.extent(data, function(d) { return d.vote; }));
 
   svg.append("g")
@@ -108,7 +122,7 @@ d3.csv("data/justice-centered/SCDB_2014_01_justiceCentered_Vote.csv", function(e
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Price ($)");
+      .text("Times voted with the majority");
 
   svg.append("path")
       .datum(data)
@@ -144,3 +158,59 @@ d3.csv("data/justice-centered/SCDB_2014_01_justiceCentered_Vote.csv", function(e
     focus.select("text").text(formatHoverText(d));
   }
 });
+
+// ** Update data section (Called from the onclick)
+function updateData() {
+    targetJustice = $("#get-name").val();
+    data = [];
+    // Get the data again
+    d3.csv("data/justice-centered/SCDB_2014_01_justiceCentered_Vote.csv", function(error, csv_data) {
+      csv_data.forEach(function(d) {
+        d.term = parseDate(d.term);
+        if (d.justiceName == targetJustice){ 
+          if (data.length == 0) {
+            var sccase = new Object();
+            sccase.date = d.term;
+            if (d.vote == 1 || d.vote == 3 || d.vote == 4) //only count consents
+              sccase.vote = 1;
+            else sccase.vote = 0;
+            sccase.totalvotes = 1;
+            data.push(sccase);
+          }
+          else if (data[data.length-1].date.getTime() != d.term.getTime()) {
+            var sccase = new Object();
+            sccase.date = d.term;
+            if (d.vote == 1 || d.vote == 3 || d.vote == 4) //only count consents
+              sccase.vote = 1;
+            else sccase.vote = 0;
+            sccase.totalvotes = 1;
+            data.push(sccase);
+          }
+          else {
+            d.vote = +d.vote;
+            if (d.vote == 1 || d.vote == 3 || d.vote == 4) //only count consents
+              data[data.length-1].vote += 1;
+            data[data.length-1].totalvotes += 1;
+          }
+        }
+      });
+
+      // Scale the range of the data again 
+      x.domain([data[0].date, data[data.length - 1].date]);
+      y.domain(d3.extent(data, function(d) { return d.vote; }));
+
+      // Select the section we want to apply our changes to
+      var svg = d3.select("body").transition();
+
+      // Make the changes
+        svg.select(".line")   // change the line
+            .duration(750)
+            .attr("d", line(data));
+        svg.select(".x.axis") // change the x axis
+            .duration(750)
+            .call(xAxis);
+        svg.select(".y.axis") // change the y axis
+            .duration(750)
+            .call(yAxis);
+    });
+}
