563690c62b4c59ecb68db14302706d4d31a5f276
George Brown <gbrown3593@gmail.com>
2015-04-19 22:40:39 -0400

    divide justices into files in new directory

---
+++ data/justice-centered/myscript.R
@@ -0,0 +1,11 @@
+justicevotes = read.csv("SCDB_2014_01_justiceCentered_Vote.csv")
+#justicevotes is the large dataframe containing data for each vote
+#Get the list of unique names
+for (name in levels(justicevotes$justiceName)){
+  #Subset the data by MP
+  tmp=subset(justicevotes,justiceName==name)
+  #Create a new filename for each MP - the folder 'mpExpenses2012' should already exist
+  fn=paste('justicevotes/',gsub(' ','',name),sep='')
+  #Save the CSV file containing separate expenses data for each MP
+  write.csv(tmp,fn,row.names=FALSE)
+}
