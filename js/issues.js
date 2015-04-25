9754bc8700d5525b009e2197ba972d4621d2ded4
Skyler <skylertom@gmail.com>
2015-04-24 23:25:07 -0400

    mostly functional autocomplete + d3 graph

--- js/issues.js
+++ js/issues.js
@@ -1,23 +1,40 @@
+/* TODO
+ * try to get the dropdown menu on the autofill to be there
+ * Error check for bad input from user
+ * write a little blurb about what this does at the top
+ * transition lines?
+ * extra:
+ * add ability to look at multiple issues
+ * add ability to look at it by court (roberts court, Rehnquist court)
+    * either shows the people in the reign of the court
+        or each court is one line
+*/
 
-  var listjustices=['AFortas', 'AJGoldberg', 'AMKennedy', 'AScalia', 'BRWhite', 'CEWhittaker', 'CThomas', 'DHSouter', 'EKagan', 'EWarren', 'FFrankfurter', 'FMurphy', 'FMVinson', 'HABlackmun', 'HHBurton', 'HLBlack', 'JGRoberts', 'JHarlan2', 'JPStevens', 'LFPowell', 'PStewart', 'RBGinsburg', 'RHJackson', 'SAAlito', 'SDOConnor', 'SFReed', 'SGBreyer', 'SMinton', 'SSotomayor', 'TCClark', 'TMarshall', 'WBRutledge', 'WEBurger', 'WHRehnquist', 'WJBrennan', 'WODouglas'];
 function init() {
-  for (var i = 0; i < listjustices.length; i++) {
+  var listjustices=['AFortas', 'AJGoldberg', 'AMKennedy', 'AScalia', 'BRWhite', 'CEWhittaker', 'CThomas', 'DHSouter', 'EKagan', 'EWarren', 'FFrankfurter', 'FMurphy', 'FMVinson', 'HABlackmun', 'HHBurton', 'HLBlack', 'JGRoberts', 'JHarlan2', 'JPStevens', 'LFPowell', 'PStewart', 'RBGinsburg', 'RHJackson', 'SAAlito', 'SDOConnor', 'SFReed', 'SGBreyer', 'SMinton', 'SSotomayor', 'TCClark', 'TMarshall', 'WBRutledge', 'WEBurger', 'WHRehnquist', 'WJBrennan', 'WODouglas'];
+  var listissues=["Criminal Procedure", "Civil Rights", "First Amendment", "Due Process", "Privacy", "Attorneys", "Unions", "Economic Activity", "Judicial Power", "Federalism", "Interstate Relations", "Federal Taxation", "Miscellaneous", "Private Action"]
+  for (var i in listjustices) {
     $('#combobox').append('<option value="'+listjustices[i]+'">'+listjustices[i]+'</option>');
     $('#get-name').append('<option value="'+listjustices[i]+'">'+listjustices[i]+'</option>');
+    $('#get-name2').append('<option value="'+listjustices[i]+'">'+listjustices[i]+'</option>');
   }
-  x();
+  for (var i in listissues) {
+    $('#get-issue').append('<option value="'+listissues[i]+'">'+listissues[i]+'</option>');
+  }
 }
 loadauto("combobox");
-loadauto("get-name")
+loadauto("get-name");
+loadauto("get-name2");
+loadauto("get-issue");
 
 var justices;
-//$("#get-name").val("JGRoberts");
-$("#get-name2").val("AScalia");
-
+/* 
+ *from old issue drop down
 $('.dropdown-toggle').dropdown();
 $('#dropdownList li').on('click', function() {
     $('#dropdownMenu1').html($(this).html());
 });
+*/
 
 var justice_names = 
 ['JGRoberts', 'AFortas'];
@@ -54,7 +71,6 @@ var issueKeys = {
   14:"Private Action"
 }
 
-function x() {
 var data = [];
 var margin = {top: 20, right: 50, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
@@ -205,13 +221,13 @@ d3.csv("data/justice-centered/SCDB_2014_01_justiceCentered_Vote.csv", function(e
       }
   }
 });
-}
 // ** Update data section (Called from the onclick)
 function updateData() {
     targetJustice = $("#get-name").val();
     targetJustice2 = $("#get-name2").val();
+    targetissue = issueKeys[$("#get-issue").val()];
+    //targetissue = issueKeys[$("#dropdownMenu1").text()];
     justice_names = [targetJustice, targetJustice2];
-    targetissue = issueKeys[$("#dropdownMenu1").text()];
     // Load the new data
     d3.csv("data/justice-centered/SCDB_2014_01_justiceCentered_Vote.csv", function(error, csv_data) {
           justices = [];
