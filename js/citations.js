0eb6796066ebb0f0325f2aabea183a0f2f160507
George Brown <gbrown3593@gmail.com>
2015-04-27 00:56:06 -0400

    finishing up citations and starting language

--- js/citations.js
+++ js/citations.js
@@ -1,51 +1,231 @@
 var nodes = {};
+var namelist = {};
+var testcase = "RICHARD B. CHENEY, VICE PRESIDENT OF THE UNITED STATES, et al. v. UNITED STATES DISTRICT COURT FOR THE DISTRICT OF COLUMBIA et al."
 function init() {
   $.get("data/citations/nametooldcite.json", function (data) {
-      namelist = Object.keys(data)
-      for (var i in namelist) {
-        $('#get-case').append('<option value="'+namelist[i]+'">'+namelist[i]+'</option>');
+      namelist = data;
+      thislist = Object.keys(namelist);
+      for (var i in thislist) {
+        $('#get-case').append('<option value="'+thislist[i]+'">'+thislist[i]+'</option>');
       }
   });
 }
-// loadauto("get-case");
-targetCase = $("#get-case").val();
 
 queue()
-    .defer(d3.json, "data/citations/adjlist.json") //next, get the master list!
-    .defer(d3.json, "data/citations/mastercaselist.json")
+    .defer(d3.json, "data/citations/theycite.json")
+    .defer(d3.json, "data/citations/citedby.json")
+    .defer(d3.json, "data/citations/mastercaselist1890-pres.json")
     .defer(d3.json, "data/citations/oldstyletodocket.json")
     .defer(d3.json, "data/citations/nametooldcite.json")
     .await(ready);
 
-function ready(error, bigjson, caselist, dockets, namestocites) {
-
+function ready(error, theycite, citedby, oldtoname, oldtodockets, namestocites) {
+// testcase = $("#get-case").combobox().val();
+thiscase = namestocites[testcase]
+console.log("in ready; case is " + testcase + ", or " + thiscase)
 links = [];
-  for(key in bigjson[testcase]){
+  for(key in theycite[thiscase]){
         var temp = {}
-        if(bigjson[testcase][key] == 1){
-          temp["source"] = caselist[testcase] || testcase
-          temp["target"] = caselist[key] || key
-          temp["type"] = "suit"
-          if(temp["source"] != temp["target"]){       //no case cites itself!!
+        temp["source"] = testcase || thiscase
+        temp["target"] = oldtoname[theycite[thiscase][key]] || theycite[thiscase][key]
+        temp["type"] = "suit"
+        if(temp["source"] != temp["target"]){       //no case cites itself!!
             links.push(temp)
           }
-        } else if (bigjson[testcase][key] == 2) {
-          temp["source"] = caselist[key] || key
-          temp["target"] = caselist[testcase] || testcase
+  }
+  for(key in citedby[thiscase]) {
+          var temp = {}
+          temp["source"] = oldtoname[citedby[thiscase][key]] || citedby[thiscase][key]
+          temp["target"] = testcase || thiscase
           temp["type"] = "licensing"
           if(temp["source"] != temp["target"]){
             links.push(temp)
           }
         }
-    }
 
   links.forEach(function(link) {
     link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
     link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
   });
+  links.fixed=true;
+  var width = 1000,
+      height = 600;
 
-  var width = 960,
-      height = 500;
+  var force = d3.layout.force()
+      .nodes(d3.values(nodes))
+      .links(links)
+      .size([width, height])
+      .linkDistance(150)
+      .charge(-500)
+      .on("tick", tick)
+      .start();
+
+  var svg = d3.select("#canvass").append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+  // Per-type markers, as they don't inherit styles.
+  svg.append("defs").selectAll("marker")
+      .data(["suit", "licensing", "resolved"])
+    .enter().append("marker")
+      .attr("id", function(d) { return d; })
+      .attr("viewBox", "0 -5 10 10")
+      .attr("refX", 15)
+      .attr("refY", -1.5)
+      .attr("markerWidth", 6)
+      .attr("markerHeight", 6)
+      .attr("orient", "auto")
+    .append("path")
+      .attr("d", "M0,-5L10,0L0,5");
+
+  var path = svg.append("g").selectAll("path")
+      .data(force.links())
+    .enter().append("path")
+      .attr("class", function(d) { return "link " + d.type; })
+      .attr("marker-end", function(d) { return "url(#" + d.type + ")"; });
+  var base = "http://caselaw.lp.findlaw.com/scripts/getcase.pl?court=us&vol=";
+  var regg = /\d+/gi;
+  var circle = svg.append("g").selectAll("circle")
+      .data(force.nodes())
+    .enter().append("circle")
+      .attr("r", 6)
+      .call(force.drag)
+            .on("click", function(d) { 
+              // REDRAW GRAPH ON CLICK OF A CIRCLE
+        console.log(d.name + "<- dname")
+        var mystr = d.name;
+        if(mystr in namestocites) {
+          console.log("changing testcase to " + mystr)
+          testcase = mystr
+        } else {
+          if(mystr in oldtoname) {
+            console.log("hi")
+              testcase = oldtoname[mystr]
+            }
+        }
+        // console.log(mystr)
+        // testcase = oldtoname[mystr]
+        // if(! testcase) {
+          // bigregg = /\d{1,3}\s{1,3}U.S.(\s|,\sat\s)\d{1,3}/gi; //same as from python code
+          // if(mystr.match(bigregg)) {
+          //   testcase = mystr.match(bigregg)[0]  //find the key for use in mastercaselist.json ERRORS HERE
+          // }
+        // }
+        d3.select("svg").remove();          //remove the last graph viz
+        // svg.selectAll("*").remove();
+        ready(error, theycite, citedby, oldtoname, oldtodockets, namestocites)       //call ready again to create the new one
+      });
+
+  var text = svg.append("g").selectAll("text")
+      .data(force.nodes())
+    .enter().append("text")
+      .attr("x", 8)
+      .attr("y", ".31em")
+      .text(function(d) { 
+        return d.name
+      })
+      .on("click", function(d){ //open link in new window -- need to fix this for cases after 1997!
+        var mystr = d.name;
+        var numlist = mystr.match(regg);
+        if(! numlist){
+          // console.log("hi")
+          try {
+            lastpart = namestocites[mystr].split("U.S. ")[1]
+            url = base + namestocites[mystr].substring(0,3) + "&invol=" + lastpart
+            // x = caselist.getKeyByValue(d.name)
+            // y = dockets[x]
+            // url = base + "000&invol=" + y
+          }
+          catch(e) {
+            try {
+              "poop" === undefined 
+            } catch(er) {
+            url = "http://findlaw.com"
+          }
+          }
+        } 
+        else {
+          url = base + numlist[0] + "&invol=" + numlist[1];
+        }
+        window.open(url);
+      });
+
+
+  // Use elliptical arc path segments to doubly-encode directionality.
+  function tick() {
+    path.attr("d", linkArc);
+    circle.attr("transform", transform);
+    text.attr("transform", transform);
+  }
+
+  function linkArc(d) {
+    var dx = d.target.x - d.source.x,
+        dy = d.target.y - d.source.y,
+        dr = Math.sqrt(dx * dx + dy * dy);
+    return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+  }
+
+  function transform(d) {
+    return "translate(" + d.x + "," + d.y + ")";
+  }
+}
+
+function testme() {
+  testcase = $("#get-case").combobox().val();
+  var nodes = {};
+  var namelist = {};
+  function init() {
+    $.get("data/citations/nametooldcite.json", function (data) {
+      namelist = data;
+      thislist = Object.keys(namelist);
+      for (var i in thislist) {
+        $('#get-case').append('<option value="'+thislist[i]+'">'+thislist[i]+'</option>');
+      }
+  });
+}
+  d3.selectAll("circle").remove();
+  d3.selectAll("text").remove();
+  d3.selectAll("svg").remove();          //remove the last graph viz
+  d3.selectAll("canvass").remove();
+  queue()
+    .defer(d3.json, "data/citations/theycite.json")
+    .defer(d3.json, "data/citations/citedby.json")
+    .defer(d3.json, "data/citations/mastercaselist1890-pres.json")
+    .defer(d3.json, "data/citations/oldstyletodocket.json")
+    .defer(d3.json, "data/citations/nametooldcite.json")
+    .await(ready);
+
+function ready(error, theycite, citedby, oldtoname, oldtodockets, namestocites) {
+// testcase = $("#get-case").combobox().val();
+thiscase = namestocites[testcase]
+console.log("in ready; case is " + testcase + ", or " + thiscase)
+links = [];
+  for(key in theycite[thiscase]){
+        var temp = {}
+        temp["source"] = testcase || thiscase
+        temp["target"] = oldtoname[theycite[thiscase][key]] || theycite[thiscase][key]
+        temp["type"] = "suit"
+        if(temp["source"] != temp["target"]){       //no case cites itself!!
+            links.push(temp)
+          }
+  }
+  for(key in citedby[thiscase]) {
+          var temp = {}
+          temp["source"] = oldtoname[citedby[thiscase][key]] || citedby[thiscase][key]
+          temp["target"] = testcase || thiscase
+          temp["type"] = "licensing"
+          if(temp["source"] != temp["target"]){
+            links.push(temp)
+          }
+        }
+
+  links.forEach(function(link) {
+    link.source = nodes[link.source] || (nodes[link.source] = {name: link.source});
+    link.target = nodes[link.target] || (nodes[link.target] = {name: link.target});
+  });
+links.fexed = true;
+  var width = 1000,
+      height = 600;
 
   var force = d3.layout.force()
       .nodes(d3.values(nodes))
@@ -79,8 +259,8 @@ links = [];
     .enter().append("path")
       .attr("class", function(d) { return "link " + d.type; })
       .attr("marker-end", function(d) { return "url(#" + d.type + ")"; });
-var base = "http://caselaw.lp.findlaw.com/scripts/getcase.pl?court=us&vol=";
-var regg = /\d+/gi;
+  var base = "http://caselaw.lp.findlaw.com/scripts/getcase.pl?court=us&vol=";
+  var regg = /\d+/gi;
   var circle = svg.append("g").selectAll("circle")
       .data(force.nodes())
     .enter().append("circle")
@@ -88,12 +268,29 @@ var regg = /\d+/gi;
       .call(force.drag)
             .on("click", function(d) { 
               // REDRAW GRAPH ON CLICK OF A CIRCLE
+        console.log(d.name + "<- dname")
         var mystr = d.name;
-        bigregg = /\d{1,3}\s{1,3}U.S.(\s|,\sat\s)\d{1,3}/gi; //same as from python code
-        targetCase = mystr.match(bigregg)[0]  //find the key for use in mastercaselist.json ERRORS HERE
+        if(mystr in namestocites) {
+          console.log("changing testcase to " + mystr)
+          testcase = mystr
+        } else {
+          if(mystr in oldtoname) {
+            console.log("hi")
+              testcase = oldtoname[mystr]
+            }
+        }
+        // console.log(mystr)
+        // testcase = oldtoname[mystr]
+        // if(! testcase) {
+          // bigregg = /\d{1,3}\s{1,3}U.S.(\s|,\sat\s)\d{1,3}/gi; //same as from python code
+          // if(mystr.match(bigregg)) {
+          //   testcase = mystr.match(bigregg)[0]  //find the key for use in mastercaselist.json ERRORS HERE
+          // }
+        // }
         d3.select("svg").remove();          //remove the last graph viz
-        ready(error,bigjson,caselist,dockets,namestocites)       //call ready again to create the new one
-         });
+        // svg.selectAll("*").remove();
+        ready(error, theycite, citedby, oldtoname, oldtodockets, namestocites)       //call ready again to create the new one
+      });
 
   var text = svg.append("g").selectAll("text")
       .data(force.nodes())
@@ -107,14 +304,20 @@ var regg = /\d+/gi;
         var mystr = d.name;
         var numlist = mystr.match(regg);
         if(! numlist){
-          console.log("hi")
+          // console.log("hi")
           try {
+            lastpart = namestocites[mystr].split("U.S. ")[1]
+            url = base + namestocites[mystr].substring(0,3) + "&invol=" + lastpart
             // x = caselist.getKeyByValue(d.name)
             // y = dockets[x]
             // url = base + "000&invol=" + y
           }
           catch(e) {
+            try {
+              "poop" === undefined 
+            } catch(er) {
             url = "http://findlaw.com"
+          }
           }
         } 
         else {
@@ -129,6 +332,7 @@ var regg = /\d+/gi;
     path.attr("d", linkArc);
     circle.attr("transform", transform);
     text.attr("transform", transform);
+
   }
 
   function linkArc(d) {
@@ -141,4 +345,6 @@ var regg = /\d+/gi;
   function transform(d) {
     return "translate(" + d.x + "," + d.y + ")";
   }
+}
+  
 }
---
+++ js/language.js
@@ -0,0 +1,53 @@
+// queue()
+  // .defer(d3.json, "data/lang/wordfreqsfinal.json")
+  // .await(ready)
+
+var word1 = "civil"
+var word2 = "freedom"
+
+function ready(er, allfreqs) {
+  // for(year = 1991; year <2009; year++) {
+
+  // }
+
+
+  var margin = {top: 20, right: 50, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+  var x = d3.time.scale()
+    .range([0, width]);
+
+  var y = d3.scale.linear()
+      .range([height, 0]);
+
+  var color = d3.scale.category10();
+
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left");
+
+  var line = d3.svg.line()
+      .defined(function(d){return d.num != null})
+      .x(function(d) { return x(d.date); })
+      .y(function(d) { return y(d.num); });
+
+  var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  d3.json("data/lang/wordfreqsfinal.json", function(err, data) {
+    data.forEach(function(d) {
+    
+    })
+  }
+
+}
+
+ready();
